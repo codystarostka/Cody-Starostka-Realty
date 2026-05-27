508ab81482afe65528f5752efeb0cad2589a729a
codystarostka <xw5gxvthyk@privaterelay.appleid.com>
2026-05-26 20:56:12 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import App from "./App.jsx";
+import { BlogList, BlogPost } from "./Blog.jsx";
+import AxonGroup from "./AxonGroup.jsx";
+import WestMichigan from "./WestMichigan.jsx";
+
+function BlogPostWrapper() {
+  var params = useParams();
+  return <BlogPost slug={params.slug} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog" element={<BlogList />} />
+        <Route path="/blog/:slug" element={<BlogPostWrapper />} />
+        <Route path="/axon-group" element={<AxonGroup />} />
+        <Route path="/west-michigan" element={<WestMichigan />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
